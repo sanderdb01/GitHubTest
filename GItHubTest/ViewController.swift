e1aebe8cfc240f840716089d1492b5259fdd4398
David Sanders <sanderdb01@gmail.com>
2015-05-27 11:54:13 -0400

    second commit

--- GItHubTest/ViewController.swift
+++ GItHubTest/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
 
     @IBAction func gitButtonPressed(sender: AnyObject) {
         println("Button pressed")
+        println("Adding another change for git test")
     }
 
 }
